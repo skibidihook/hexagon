5194c6b6c6e667f1be76c521acc638eeaf0de2ee
skibidihook <skibidihook@gmail.com>
2025-02-21 12:38:45 +0000

    Update UILibrary.lua

--- scripts/UILibrary.lua
+++ scripts/UILibrary.lua
@@ -1,5 +1,3 @@
-local Mouse = cloneref(LocalPlayer:GetMouse())
-local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 -- Hexagon UI Library
 local TweenService, RunService, UserInputService,gui,dragging,dragInput,dragStart,startPos,cpt,cpf,cppicking,cppickingVal,cppickingAlpha,cphue,cpsat,cpval,focused,highest,focusedBox = game:GetService("TweenService"),game:GetService("RunService"), game:GetService("UserInputService")
 local cpalpha = 0
@@ -1862,7 +1860,7 @@ function library:CreateWindow(csize, cpos)
 						repeat 
 							wait()
 							
-							local mouse = Mouse
+							local mouse = local mouse = game:GetService("Players").LocalPlayer:GetMouse()
 							local obj = color.hueselector
 							local pos = (mouse.Y-obj.AbsolutePosition.Y)/obj.AbsoluteSize.Y
 							pos = (pos < 0 and 0) or (pos > 1 and 1) or pos
@@ -1888,7 +1886,7 @@ function library:CreateWindow(csize, cpos)
 						repeat 
 							wait()
 							
-							local mouse = Mouse
+							local mouse = local mouse = game:GetService("Players").LocalPlayer:GetMouse()
 							local obj = color.hueselector
 							local x = (mouse.X-obj.AbsolutePosition.X)/obj.AbsoluteSize.X
 							local y = (mouse.Y-obj.AbsolutePosition.Y)/obj.AbsoluteSize.Y
